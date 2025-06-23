fb5fd8124a16e58f256016af442ae70fa84c216b
mahmoud.aboelsoud <mahmoud.aboelsoud@servicenow.com>
2025-06-23 22:35:51 +0300

    Init Spring boot application with (MVC, JPA, Lombok) libraries

---
+++ src/main/java/org/gs1eg/business_rule_engine/BusinessRuleEngineApplication.java
@@ -0,0 +1,13 @@
+package org.gs1eg.business_rule_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessRuleEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessRuleEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/gs1eg/business_rule_engine/BusinessRuleEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gs1eg.business_rule_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessRuleEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
