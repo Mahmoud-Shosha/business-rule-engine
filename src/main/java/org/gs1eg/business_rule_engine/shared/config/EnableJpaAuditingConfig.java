a1a9a7709790ad286cbca93e6a57c1eb6880bf63
mahmoud.aboelsoud <mahmoud.aboelsoud@servicenow.com>
2025-06-24 14:38:31 +0300

    Enable JPA auditing (Configuration)

---
+++ src/main/java/org/gs1eg/business_rule_engine/shared/config/EnableJpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package org.gs1eg.business_rule_engine.shared.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class EnableJpaAuditingConfig {
+}
